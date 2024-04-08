8f25aa30442aaec5e57dbd841d9fe98ece8ea9c3
Benjamin Regler <benjamin.regler@sz.de>
2024-04-08 14:00:25 +0200

    Ausgabe geändert

--- QuizGame/Program.vb
+++ QuizGame/Program.vb
@@ -2,7 +2,7 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hallo Welt!")
+        Console.WriteLine("Das ist ein Text!")
         Console.ReadLine()
     End Sub
 End Module
